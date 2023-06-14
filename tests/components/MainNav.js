e7ac6c4586bce5d6565fb224abe3c8719dc3fdc5
kirillzhakin <kirillzhakin@mail.ru>
2023-06-14 11:41:22 +0500

    add test nav

--- tests/components/MainNav.js
+++ tests/components/MainNav.js
@@ -8,4 +8,17 @@ describe('MainNav', () => {
     const companyName = screen.getByText('Lexicon Careers')
     expect(companyName).toBeInTheDocument()
   })
+  it('Отобразить меню навигации', () => {
+    render(MainNav)
+    const navigationMenuItems = screen.getAllByRole('listitem')
+    const navigationMenuText = navigationMenuItems.map(item => item.textContent)
+    expect(navigationMenuText).toEqual([
+      'Teams',
+      'Locations',
+      'Life at Lexicon',
+      'How we hire',
+      'Students',
+      'Jobs'
+    ])
+  })
 })
