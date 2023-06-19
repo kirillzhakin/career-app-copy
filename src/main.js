569dc5189eb5fbeb23f158c8002f0975f08bd711
Kirill Zhakin <kirillzhakin@mail.ru>
2023-06-19 09:08:14 +0500

    Revert "Feat/4 add sub nav"

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
-import App from '@/App.vue'
-import '@/index.css'
+import App from './App.vue'
 
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faSearch)
-
-createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App).mount('#app')
