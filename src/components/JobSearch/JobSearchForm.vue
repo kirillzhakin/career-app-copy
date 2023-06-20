3d1481d9275c28f5172c20649694efd334fda641
kirillzhakin <kirillzhakin@mail.ru>
2023-06-20 20:12:08 +0500

    add text input

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -5,12 +5,7 @@
     <div class="flex h-full flex-1 flex-nowrap text-base font-light">
       <div class="relative flex h-full flex-1 items-center pr-3">
         <label class="absolute -top-10 left-0">Role</label>
-        <input
-          v-model="role"
-          type="text"
-          placeholder="Software engineer"
-          class="w-full text-lg font-normal focus:outline-none"
-        />
+        <text-input v-model="role" placeholder="Software engineer" />
       </div>
       <span
         class="border- flex h-full items-center border-l border-r border-brand-gray-3 bg-brand-gray-2 px-3"
@@ -18,12 +13,7 @@
       >
       <div class="relative flex h-full flex-1 items-center pl-3">
         <label class="absolute -top-10 left-0">Where?</label>
-        <input
-          v-model="location"
-          type="text"
-          placeholder="Los Angeles"
-          class="w-full text-lg font-normal focus:outline-none"
-        />
+        <text-input v-model="location" placeholder="Los Angeles" />
       </div>
 
       <action-button text="Search" type="secondary" class="rounded-r-3xl" />
@@ -33,11 +23,12 @@
 
 <script>
 import ActionButton from '@/components/Shared/ActionButton.vue'
+import TextInput from '@/components/Shared/TextInput.vue'
 
 export default {
   name: 'JobSearchForm',
 
-  components: { ActionButton },
+  components: { ActionButton, TextInput },
   data() {
     return {
       role: '',
